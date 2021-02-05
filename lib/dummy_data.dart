870412f0a780644974f143aa18fc0689b3bd23da
Sahajpal <sahajpal80@gmail.com>
2021-02-05 23:45:05 +0530

    Made Dummy prototype of Category Screen

---
+++ lib/dummy_data.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.green,
+  ),
+];
