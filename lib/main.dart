a751c93c3d9ab6a44ed3f2d428ae3576299b64e2
Sahajpal <sahajpal80@gmail.com>
2021-02-08 00:53:37 +0530

    Beautified Meal List and added detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'file:///C:/Users/Sahaj/Desktop/projects/meals_app/lib/screens/categories_screen.dart';
-import 'file:///C:/Users/Sahaj/Desktop/projects/meals_app/lib/screens/category_meals_screen.dart';
+
+import './screens/categories_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/meal_detail_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -32,6 +34,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
     );
   }
